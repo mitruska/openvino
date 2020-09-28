6a51d146fa795d6e7c06dfe1d5cb6e62fce54bb8
mitruska <katarzyna.mitrus@intel.com>
2020-09-29 01:58:17 +0200

    Custom ReorgYolo implementation

--- ngraph/core/reference/src/runtime/reference/reorg_yolo.cpp
+++ ngraph/core/reference/src/runtime/reference/reorg_yolo.cpp
@@ -28,6 +28,7 @@ namespace ngraph
     {
         namespace reference
         {
+            // Custom ReorgYolo - like ExtractImagePatches
             void reorg_yolo(const char* arg,
                             char* out,
                             const Shape& in_shape,
@@ -40,38 +41,30 @@ namespace ngraph
                 size_t in_H = in_shape[2];
                 size_t in_W = in_shape[3];
 
-                // Inferce output shape logic:
+                // Inference output shape logic:
                 // in_shape [N,C,H,W] -> out_shape [N, C*(stride*stride), H/stride, W/stride]
-                // ReorgYolo implementation calculates indexes like for backprop:
-                // in_shape [N,C,H,W] -> out_shape [N, C/(stride*stride), H*stride, W*stride]
 
-                size_t impl_out_C = in_C / (stride * stride);
-                if (impl_out_C == 0)
-                {
-                    throw ngraph_error(
-                        "ReorgYolo. For [N, C, H, W] input shape, C >= (stride*stride) is "
-                        "required.");
-                }
-                size_t impl_out_H = in_H * stride;
-                size_t impl_out_W = in_W * stride;
+                size_t out_N = in_shape[0];
+                size_t out_C = in_shape[1] * (stride * stride);
+                size_t out_H = in_shape[2] / stride;
+                size_t out_W = in_shape[3] / stride;
 
-                for (size_t n = 0; n < in_N; ++n)
+                for (size_t n = 0; n < out_N; ++n)
                 {
-                    for (size_t c = 0; c < in_C; ++c)
+                    for (size_t c = 0; c < out_C; ++c)
                     {
-                        for (size_t h = 0; h < in_H; ++h)
+                        for (size_t h = 0; h < out_H; ++h)
                         {
-                            for (size_t w = 0; w < in_W; ++w)
+                            for (size_t w = 0; w < out_W; ++w)
                             {
-                                size_t offset = c / impl_out_C;
-                                size_t impl_c = c % impl_out_C;
-                                size_t impl_h = h * stride + offset / stride;
-                                size_t impl_w = w * stride + offset % stride;
+                                size_t offset = c / in_C;
+                                size_t arg_c = c % in_C;
+                                size_t arg_h = h * stride + offset / stride;
+                                size_t arg_w = w * stride + offset % stride;
 
                                 size_t arg_index =
-                                    ((n * impl_out_C + impl_c) * impl_out_H + impl_h) * impl_out_W +
-                                    impl_w;
-                                size_t dest_index = ((n * in_C + c) * in_H + h) * in_W + w;
+                                    ((n * in_C + arg_c) * in_H + arg_h) * in_W + arg_w;
+                                size_t dest_index = ((n * out_C + c) * out_H + h) * out_W + w;
 
                                 arg_index *= elem_size;
                                 dest_index *= elem_size;
@@ -84,6 +77,69 @@ namespace ngraph
                     }
                 }
             }
+
+            //             // Original ReorgYolo - like plugins
+            //             void reorg_yolo(const char* arg,
+            //                             char* out,
+            //                             const Shape& in_shape,
+            //                             int64_t stride,
+            //                             const size_t elem_size)
+            //             {
+            //                 // [N, C, H, W]
+            //                 size_t in_N = in_shape[0];
+            //                 size_t in_C = in_shape[1];
+            //                 size_t in_H = in_shape[2];
+            //                 size_t in_W = in_shape[3];
+
+            //                 // Inference output shape logic:
+            //                 // in_shape [N,C,H,W] -> out_shape [N, C*(stride*stride), H/stride,
+            //                 W/stride]
+            //                 // ReorgYolo implementation calculates indexes like for backprop:
+            //                 // in_shape [N,C,H,W] -> out_shape [N, C/(stride*stride), H*stride,
+            //                 W*stride]
+
+            //                 size_t impl_out_C = in_C / (stride * stride);
+            //                 if (impl_out_C == 0)
+            //                 {
+            //                     throw ngraph_error(
+            //                         "ReorgYolo. For [N, C, H, W] input shape, C >=
+            //                         (stride*stride) is "
+            //                         "required.");
+            //                 }
+            //                 size_t impl_out_H = in_H * stride;
+            //                 size_t impl_out_W = in_W * stride;
+
+            //                 for (size_t n = 0; n < in_N; ++n)
+            //                 {
+            //                     for (size_t c = 0; c < in_C; ++c)
+            //                     {
+            //                         for (size_t h = 0; h < in_H; ++h)
+            //                         {
+            //                             for (size_t w = 0; w < in_W; ++w)
+            //                             {
+            //                                 size_t offset = c / impl_out_C;
+            //                                 size_t impl_c = c % impl_out_C;
+            //                                 size_t impl_h = h * stride + offset / stride;
+            //                                 size_t impl_w = w * stride + offset % stride;
+
+            //                                 size_t arg_index =
+            //                                     ((n * impl_out_C + impl_c) * impl_out_H + impl_h)
+            //                                     * impl_out_W +
+            //                                     impl_w;
+            //                                 size_t dest_index = ((n * in_C + c) * in_H + h) *
+            //                                 in_W + w;
+
+            //                                 arg_index *= elem_size;
+            //                                 dest_index *= elem_size;
+
+            //                                 std::copy(arg + arg_index,
+            //                                           arg + (arg_index + elem_size),
+            //                                           out + dest_index);
+            //                             }
+            //                         }
+            //                     }
+            //                 }
+            //             }
         }
     }
 }
